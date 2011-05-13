cf0beb06b55170663f6abfee1fd1a4d3580369d5
johndoe9 <john.doe9@gmx.de>
2011-05-13 20:08:40 +0200

    - neuer Test fuer exakte Checkout Regel

--- intermission-ball/src/test/java/de/fuhlsfield/game/rule/ExactCheckoutRuleCheckTest.java
+++ intermission-ball/src/test/java/de/fuhlsfield/game/rule/ExactCheckoutRuleCheckTest.java
@@ -19,8 +19,8 @@ import de.fuhlsfield.game.GameConfig;
 import de.fuhlsfield.game.Player;
 import de.fuhlsfield.game.score.StandardScoreCalculator;
 
-public class EachBallAtLeastOnceRuleCheckTest {
-
+public class ExactCheckoutRuleCheckTest {
+	
 	private static final BallValue BALL_TWO_POINTS = new BallValue(Ball.BUNTI, 2);
 	private static final BallValue BALL_THREE_POINTS = new BallValue(Ball.BASKI, 3);
 	private static final BallValue BALL_FOUR_POINTS = new BallValue(Ball.TISCHI_BALLI, 4);
@@ -37,30 +37,39 @@ public class EachBallAtLeastOnceRuleCheckTest {
 	}
 	
 	@Test
-	public void thatIsAttemptPossibleWhenScoreGreaterTargetPoints () {
+	public void thatIsAttemptPossibleWhenExactCheckout () {
 		this.game.check(BALL_TWO_POINTS.getBall(), PLAYER, true);
 		this.game.check(BALL_THREE_POINTS.getBall(), PLAYER, true);
-		when(this.gameConfig.getTargetPoints()).thenReturn(8);
+		when(this.gameConfig.getTargetPoints()).thenReturn(9);
 		assertTrue(createClassUnderTest().isAttemptPossible(new Attempt(PLAYER, BALL_FOUR_POINTS.getBall()), this.game));
 	}
 	
 	@Test
-	public void thatIsAttemptPossibleWhenScoreEqualsTargetPoints () {
+	public void thatIsAttemptPossibleWhenAfterAttemptExactCheckoutPossible () {
 		this.game.check(BALL_TWO_POINTS.getBall(), PLAYER, true);
 		this.game.check(BALL_THREE_POINTS.getBall(), PLAYER, true);
-		when(this.gameConfig.getTargetPoints()).thenReturn(9);
-		assertTrue(createClassUnderTest().isAttemptPossible(new Attempt(PLAYER, BALL_FOUR_POINTS.getBall()), this.game));
+		when(this.gameConfig.getTargetPoints()).thenReturn(10);
+		assertTrue(createClassUnderTest().isAttemptPossible(new Attempt(PLAYER, BALL_THREE_POINTS.getBall()), this.game));
 	}
-
+	
 	@Test
-	public void thatIsAttemptNotPossibleWhenTargetPointsIsAccomplishedWithoutAllBalls () {
-		this.game.check(BALL_FOUR_POINTS.getBall(), PLAYER, true);
-		when(this.gameConfig.getTargetPoints()).thenReturn(9);
+	public void thatIsAttemptNotPossibleWhenAfterAttemptNoExactCheckoutPossible () {
+		this.game.check(BALL_TWO_POINTS.getBall(), PLAYER, true);
+		this.game.check(BALL_THREE_POINTS.getBall(), PLAYER, true);
+		when(this.gameConfig.getTargetPoints()).thenReturn(10);
+		assertFalse(createClassUnderTest().isAttemptPossible(new Attempt(PLAYER, BALL_FOUR_POINTS.getBall()), this.game));
+	}
+	
+	@Test
+	public void thatIsAttemptNotPossibleWhenMorePointsThanNeeded () {
+		this.game.check(BALL_TWO_POINTS.getBall(), PLAYER, true);
+		this.game.check(BALL_THREE_POINTS.getBall(), PLAYER, true);
+		when(this.gameConfig.getTargetPoints()).thenReturn(8);
 		assertFalse(createClassUnderTest().isAttemptPossible(new Attempt(PLAYER, BALL_FOUR_POINTS.getBall()), this.game));
 	}
 
-	private EachBallAtLeastOnceRuleCheck createClassUnderTest () {
-		return new EachBallAtLeastOnceRuleCheck();
+	private ExactCheckoutRuleCheck createClassUnderTest () {
+		return new ExactCheckoutRuleCheck();
 	}
 
 }
